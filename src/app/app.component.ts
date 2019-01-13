bb64f94b8d930dc16170b605de3bea69dffcf20f
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-01-13 04:41:32 -0200

    Aguardando verificação de login antes de definir a página principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { LoginPage } from '../pages/login/login';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = LoginPage;
+  rootPage: any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, cache: CacheService, afAuth: AngularFireAuth) {
     afAuth.auth.useDeviceLanguage();
