e394ffa91a072b16791ef0eb68cc1e963eca5c4d
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-03-04 04:04:37 -0300

    Criando interface

---
+++ src/models/refeicao.ts
@@ -0,0 +1,11 @@
+export interface Refeicao {
+    'data': string,
+    'dia-semana': string,
+    'refeicao': string,
+    'prato': string,
+    'guarnicao': string,
+    'arroz': string,
+    'feijao': string,
+    'salada': string,
+    'sobremesa': string
+}
